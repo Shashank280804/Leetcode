2174243bf7dc10824105c04ac0a5fd993cccd180
shashank <116015929+Shashank280804@users.noreply.github.com>
2024-10-10 22:08:40 +0530

    find pivot index code

---
+++ findPivotIndex.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int rightSum = accumulate(nums.begin(), nums.end(), 0);
+        int leftSum = 0;
+        for (int idx = 0; idx < nums.size(); idx++) {
+            rightSum -= nums[idx];
+            
+            if (leftSum == rightSum)
+                return idx;
+           
+            leftSum += nums[idx];
+        }
+        return -1;
+    }
+};
