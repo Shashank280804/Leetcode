b24c57f02d62cb8b04ba8d9ccfd61a90a0f3e5c7
shashank <116015929+Shashank280804@users.noreply.github.com>
2024-10-13 16:30:19 +0530

    Added some modifications

--- nextGreaterElement.cpp
+++ nextGreaterElement.cpp
@@ -1,3 +1,18 @@
+/*
+Algorithm:
+1. Initialize Result Array: Start with an array arr filled with -1, assuming no greater elements are found.
+
+2. Outer Loop: Iterate through each element in nums1:
+
+3. For each element in nums1, locate its position in nums2.
+4. Inner Loop: Once a match is found in nums2:
+
+5. Start a second loop from the next index in nums2 to find the first greater element.
+6. If a greater element is found, update the corresponding index in arr.
+7. If the end of nums2 is reached without finding a greater element, arr remains -1 for that index.
+8. Return the Result: After processing all elements in nums1, return the result array arr.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
